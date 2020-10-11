6d5f2c4d63dbb4268b4f5294b8262ce4bc8ce6e1
Ahmed Aldhafeeri <ar.aldhafeeri11@gmail.com>
2020-10-11 13:57:32 +0300

    .

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kajlkafsdljhdsfaljkfhljk3398339p83293unw'
 port = int(os.environ.get('PORT', 5000))
+circles_data = Data()
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -17,7 +18,6 @@ def context_processor():
     return dict(circles_data= circles_data.get_circles_data())
 
 if __name__ == "__main__":
-    circles_data = Data()
     circles_data.plot_maps()
     circles_data.plot_bars()
     circles_data.plot_lines()
