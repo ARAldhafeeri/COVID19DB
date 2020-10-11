a580bef3e8d3697db4da989e7709aa6948e659fb
Ahmed Aldhafeeri <ar.aldhafeeri11@gmail.com>
2020-10-11 11:06:32 +0300

    server

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, render_template
 from data import Data
-app = Flask(__name__, template_folder="templates")
+import os
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kajlkafsdljhdsfaljkfhljk3398339p83293unw'
-
+port = int(os.environ.get('PORT', 5000))
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -20,4 +21,4 @@ if __name__ == "__main__":
     circles_data.plot_maps()
     circles_data.plot_bars()
     circles_data.plot_lines()
-    app.run()
+    app.run(debug=True, port=port)
